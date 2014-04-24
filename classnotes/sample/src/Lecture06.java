24b41c6caae1745626b8ca4fbe1569bdb3963225
mjelks <michael@splurgy.com>
2014-04-23 19:05:36 -0700

    pull data

---
+++ classnotes/sample/src/Lecture06.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mjelks
+ */
+public class Lecture06 {
+  public static void main(String[] args) {
+    
+  }
+  
+  // sample of simple class method
+  public static long mutiply(int n1, int n2) {
+    long product;
+    
+    product = n1 * n2;
+    
+    return product;
+  }
+}
--- lab2/src/CashRegister/InventoryItem.java
+++ lab2/src/CashRegister/InventoryItem.java
@@ -1,19 +1,13 @@
 /*
-* @author mjelks
-*/
-
+ * @author mjelks
+ */
 package CashRegister;
 
-import java.util.HashMap;
-
 public class InventoryItem {
-  //HashMap <String, String> myMap = new HashMap<String, String>();
-  String  productDescription = "";
-  double  productPrice = 0.0;
-  
+  String productDescription = "";
+  double productPrice = 0.0;
+
   InventoryItem(String productDescription, double productPrice) {
-	//myMap.put("description", productDescription);
-	//myMap.put("price", String.valueOf(productPrice));
 	this.productDescription = productDescription;
 	this.productPrice = productPrice;
   }
